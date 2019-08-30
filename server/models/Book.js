0dac7e07f8682ea6f5f72a0c42aee219a06f1585
ikesh pack <packappsforyou@gmail.com>
2019-08-30 14:00:24 -0400

    Add Book model

---
+++ server/models/Book.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  authors: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "authors"
+    }
+  ],
+  rating: {
+    type: Number
+  },
+  coverPhoto: {
+    type: String,
+    required: true
+  },
+  coverType: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  publishDate: {
+    type: Date,
+    required: true
+  },
+  publisher: {
+    type: Schema.Types.ObjectId,
+    ref: "publishers"
+  },
+  genres: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "genres"
+    }
+  ],
+  ratings: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "ratings"
+    }
+  ],
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "reviews"
+    }
+  ],
+  series: {
+    type: Schema.Types.ObjectId,
+    ref: "series"
+  },
+  questions: [
+    {
+      type: Schema.Types.ObjectId,
+      ref
+    }
+  ],
+  characters: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "characters"
+    }
+  ],
+  edition: {
+    type: String,
+    required: true
+  },
+  pages: {
+    type: Number,
+    required: true
+  },
+  isbn: {
+    type: String,
+    required: true
+  },
+  setting: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "settings"
+    }
+  ]
+});
+
+module.exports = mongoose.model("books", BookSchema);
