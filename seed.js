6a347a9f2c7a6475b6108fd480fbd5fb13c10ea8
ikesh pack <packappsforyou@gmail.com>
2019-08-30 17:06:37 -0400

    Add seeding for demo user

--- seed.js
+++ seed.js
@@ -1,8 +1,26 @@
 const faker = require("faker");
-const User = require("./models/User");
-const Board = require("./models/Board");
-const Image = require("./models/Image");
-const Pin = require("./models/Pin");
+const User = require("./server/models/User");
 const bcrypt = require("bcryptjs");
-const db = require("./config/keys").mongoURI;
+const db = require("./config/keys").MONGO_URI;
 const mongoose = require("mongoose");
+
+mongoose.connect(db, { useNewUrlParser: true }).then(async () => {
+  console.log("Connected to MongoDB successfully");
+
+  const demoUserData = {
+    email: "DemoUser@badreads.com",
+    password: "test123",
+    name: "DemoUser"
+  };
+
+  const demoUser = await User.findOne({ email: "DemoUser@badreads.com" });
+
+  if (!demoUser) {
+    const hashedPassword = await bcrypt.hash(demoUserData.password, 10);
+    demoUserData.password = hashedPassword;
+    await new User(demoUserData).save();
+    console.log("Demo User Saved");
+  } else {
+    console.log("No Demo User Needed");
+  }
+});
