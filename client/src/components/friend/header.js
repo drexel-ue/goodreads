4c36f38d0c5ca86fb2686e6539708c31cb83c662
ikesh pack <packappsforyou@gmail.com>
2019-09-02 12:45:39 -0400

    Add nav functionality

--- client/src/components/friend/header.js
+++ client/src/components/friend/header.js
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import "./header.scss";
 
-export default withRouter(({ match, location }) => {
-  const borderStyle = path =>
-    match.path === path ? { borderBottom: "2px solid black" } : {};
+export default withRouter(
+  class Header extends Component {
+    render() {
+      const borderStyle = path =>
+        this.props.location.pathname === path
+          ? { borderBottom: "2px solid black" }
+          : {};
 
-  return (
-    <div className="friends_header">
-      <div className="header_title">Friends</div>
-      <div className="make_show_buttons">
-        <div className="make_show_button" style={borderStyle("/friend")}>
-          Friends
+      const navTo = path => {
+        return () => this.props.history.push(path);
+      };
+
+      return (
+        <div className="friends_header">
+          <div className="header_title">Friends</div>
+          <div className="make_show_buttons">
+            <div
+              className="make_show_button"
+              style={borderStyle("/friend")}
+              onClick={navTo("/friend")}
+            >
+              Friends
+            </div>
+            <div
+              className="make_show_button"
+              style={borderStyle("/friend/invite")}
+              onClick={navTo("/friend/invite")}
+            >
+              Add Friends
+            </div>
+          </div>
         </div>
-        <div className="make_show_button" style={borderStyle("/friend/invite")}>
-          Add Friends
-        </div>
-      </div>
-    </div>
-  );
-});
+      );
+    }
+  }
+);
