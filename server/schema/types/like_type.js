04b73a7673ea4cc0c83a50007fdc8ac4c2510bcc
Eugene Birondo <jbirondo@gmail.com>
2019-09-02 09:47:06 -0700

    Finished create and delete mutations

--- server/schema/types/like_type.js
+++ server/schema/types/like_type.js
@@ -3,13 +3,20 @@ const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLID } = graphql;
 const UserType = require("./user_type")
 const BookType = require("./book_type")
+const CommentType = require("./comment_type")
+const QuestionType = require("./question_type")
+const AnswerType = require("./answer_type")
+
 
 const LikeType = new GraphQLObjectType({
     name: "LikeType",
     fields: () => ({
         _id: { type: GraphQLID },
         user: { type: UserType },
-        book: { type: BookType }
+        book: { type: BookType },
+        comment: { type: CommentType },
+        question: { type: QuestionType },
+        answers: { type: AnswerType }
     })
 })
 
