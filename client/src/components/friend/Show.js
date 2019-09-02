2816456b57a294def7f989b03189f51b477cb436
ikesh pack <packappsforyou@gmail.com>
2019-09-02 14:05:28 -0400

    Place search bar and submit button

--- client/src/components/friend/Show.js
+++ client/src/components/friend/Show.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
 import "./Show.scss";
 
-export default () => {
-  return (
-    <div className="show">
-      <div className="main">snlknslksns</div>
-      <div className="side">sjlsjlks</div>
-    </div>
-  );
-};
+export default class Show extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      queryString: ""
+    };
+
+    this.handleInput=this.handleInput.bind(this)
+  }
+
+  handleInput(event) {
+    event.preventDefault();
+    this.setState({ queryString: event.currentTarget.value });
+  }
+
+  render() {
+    return (
+      <div className="show">
+        <div className="main">
+          <div className="search_row">
+            <input
+              type="text"
+              className="search_bar"
+              value={this.state.queryString}
+              onChange={this.handleInput}
+              placeholder="Search Your Friend List"
+            />
+            <div className="submit">Search</div>
+          </div>
+        </div>
+        <div className="side">sjlsjlks</div>
+      </div>
+    );
+  }
+}
