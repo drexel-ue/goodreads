ab456ad4200620868c587b86f997b94c0e3cc4fe
ikesh pack <packappsforyou@gmail.com>
2019-09-03 11:46:38 -0400

    Add book total

--- client/src/components/friend/FriendTile.js
+++ client/src/components/friend/FriendTile.js
@@ -3,14 +3,22 @@ import { withRouter } from "react-router-dom";
 import "./FriendTile.scss";
 
 export default withRouter(({ history, location, user }) => {
-  console.log(user)
+  const sumBooks = () => {
+    let sum = 0;
+    user.shelves.forEach(shelf => {
+      sum += shelf.books.length;
+    });
+    return sum;
+  };
+
+  console.log(user);
   return (
     <div className="friend_tile">
       <img className="profile" src={user.profilePhoto} alt="profile" />
       <div className="user_info">
         <div className="name">{user.name}</div>
         <div className="books_and_friends">
-          <div className="books"></div>
+          <div className="books">{sumBooks()} books | </div>
           <div className="friends"></div>
         </div>
       </div>
