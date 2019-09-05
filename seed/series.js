6e82ee1967afe0882465c27c54ca918e70621e0c
ikesh pack <packappsforyou@gmail.com>
2019-09-05 14:50:15 -0400

    Remove seriesmodel and rework seed

--- seed/series.js
+++ seed/series.js
@@ -1,6 +1,6 @@
 const faker = require("faker");
 
-export default [
+module.exports = [
   faker.random.words(faker.random.number({ min: 1, max: 5 })),
   faker.random.words(faker.random.number({ min: 1, max: 5 })),
   faker.random.words(faker.random.number({ min: 1, max: 5 })),
