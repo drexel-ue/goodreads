d11a25230f136431d57f8ccc9ab0bd455d30b5f5
ikesh pack <packappsforyou@gmail.com>
2019-09-05 13:59:59 -0400

    Place title

--- client/src/components/book/BookShow.js
+++ client/src/components/book/BookShow.js
@@ -18,34 +18,40 @@ export default withRouter(({ match, history }) => {
         console.log("data", data);
         const { book } = data;
 
-        return (
-          <div className="book_show">
-            <div className="section_1">
-              <img
-                className="main_cover"
-                src={book.coverPhoto}
-                alt="book cover"
-              />
-              <div className="shelf_button">
-                <div className="want_to_read">Want To Read</div>
-                <div className="dropdown_button">
-                  <i className="fas fa-caret-down"></i>
-                </div>
-              </div>
-              <div className="rate_text">Rate this book</div>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-              </div>
-              <div className="preview">
-                <i className="fas fa-book-open"></i>
-                <div className="text">Preview</div>
+        const section1 = (
+          <div className="section_1">
+            <img
+              className="main_cover"
+              src={book.coverPhoto}
+              alt="book cover"
+            />
+            <div className="shelf_button">
+              <div className="want_to_read">Want To Read</div>
+              <div className="dropdown_button">
+                <i className="fas fa-caret-down"></i>
               </div>
             </div>
-            <div className="section_2"></div>
+            <div className="rate_text">Rate this book</div>
+            <div className="stars">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+            </div>
+            <div className="preview">
+              <i className="fas fa-book-open"></i>
+              <div className="text">Preview</div>
+            </div>
+          </div>
+        );
+
+        return (
+          <div className="book_show">
+            {section1}
+            <div className="section_2">
+              <div className="title">{book.title}</div>
+            </div>
             <div className="section_3"></div>
           </div>
         );
