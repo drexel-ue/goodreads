b668fbd1db49671e1ae8741ca72c9f7e032992b4
ikesh pack <packappsforyou@gmail.com>
2019-09-23 19:50:21 -0400

    Syntax fixes

--- client/src/components/friend/header.js
+++ client/src/components/friend/header.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-import "./header.scss";
+import "./Header.scss";
 
 export default withRouter(
   class Header extends Component {
