e0d594dc0572bfd4e0375fdc5227907434559400
ikesh pack <packappsforyou@gmail.com>
2019-09-23 17:29:15 -0400

    Add timout to browse section

--- client/src/components/nav/Nav.js
+++ client/src/components/nav/Nav.js
@@ -3,7 +3,7 @@ import { Link, withRouter } from "react-router-dom";
 import { Query, ApolloConsumer } from "react-apollo";
 import Queries from "../../graphql/queries";
 import Login from "../session/Login";
-import './Nav.css';
+import "./Nav.css";
 const { IS_LOGGED_IN } = Queries;
 
 class Nav extends React.Component {
@@ -11,13 +11,32 @@ class Nav extends React.Component {
     super(props);
 
     this.state = {
-      browseClicked: false,
+      showBrowseDropdown: false,
       commClicked: false,
       userClicked: false
     };
+
+    this.timer = undefined;
+
+    this.showDropdown = this.showDropdown.bind(this);
+    this.hideDropdown = this.hideDropdown.bind(this);
   }
 
-  render () {
+  showDropdown(event) {
+    event.preventDefault();
+    clearTimeout(this.timer);
+    this.setState({ showBrowseDropdown: true });
+  }
+
+  hideDropdown(event) {
+    event.preventDefault();
+    this.timer = setTimeout(
+      () => this.setState({ showBrowseDropdown: false }),
+      1
+    );
+  }
+
+  render() {
     return (
       <ApolloConsumer>
         {client => (
@@ -25,58 +44,65 @@ class Nav extends React.Component {
             {({ data }) => {
               if (data.isLoggedIn) {
                 return (
-                  <div className='navbar'>
-                    <div className='navbar-contents'>
-                      <Link to='/' className='in-logo'>bad</Link><Link to='/' className='in-logo2'>reads</Link>
-                      <nav className='main-nav'>
-                        <ul className='nav-list'>
-                          <li className='nav-list-item'>
-                            <Link to='/' className='nav-link'>Home</Link>
+                  <div className="navbar">
+                    <div className="navbar-contents">
+                      <Link to="/" className="in-logo">
+                        bad
+                      </Link>
+                      <Link to="/" className="in-logo2">
+                        reads
+                      </Link>
+                      <nav className="main-nav">
+                        <ul className="nav-list">
+                          <li className="nav-list-item">
+                            <Link to="/" className="nav-link">
+                              Home
+                            </Link>
                           </li>
-                          <li className='nav-list-item'>
-                            <Link to='/bookshelf/all' className='nav-link'>My Books</Link>
+                          <li className="nav-list-item">
+                            <Link to="/bookshelf/all" className="nav-link">
+                              My Books
+                            </Link>
                           </li>
-                          <li className='nav-list-item'>
+                          <li className="nav-list-item">
                             <div className="dropdown">
-                              <button className="dropbtn"
-                                onClick={e => {
-                                  e.preventDefault();
-
-                                  if (!this.state.browseClicked) {
-                                    this.setState({
-                                      browseClicked: true,
-                                      commClicked: false,
-                                      userClicked: false
-                                    });
-                                  } else {
-                                    this.setState({
-                                      browseClicked: false
-                                    });
-                                  }
-                                }}>Browse</button>
+                              <button
+                                className="dropbtn"
+                                onMouseEnter={this.showDropdown}
+                                onMouseLeave={this.hideDropdown}
+                              >
+                                Browse
+                              </button>
                               <div
+                                onMouseEnter={this.showDropdown}
+                                onMouseLeave={this.hideDropdown}
                                 className={`dropdown-content ${
-                                  this.state.browseClicked ? "reveal" : "hide"
-                                  }`}
+                                  this.state.showBrowseDropdown
+                                    ? "reveal"
+                                    : "hide"
+                                }`}
                               >
                                 <div className="dropdown-item">
-                                  <Link to='/recommendations'>Recommendations</Link>
+                                  <Link to="/recommendations">
+                                    Recommendations
+                                  </Link>
                                 </div>
                                 <div className="dropdown-item">
-                                  <Link to='/new_releases'>New Releases</Link>
+                                  <Link to="/new_releases">New Releases</Link>
                                 </div>
                                 <div className="dropdown-item">
-                                  <Link to='/list'>Lists</Link>
+                                  <Link to="/list">Lists</Link>
                                 </div>
                                 <div className="dropdown-item">
-                                  <Link to='/book'>Explore</Link>
+                                  <Link to="/book">Explore</Link>
                                 </div>
                               </div>
                             </div>
                           </li>
-                          <li className='nav-list-item'>
+                          <li className="nav-list-item">
                             <div className="dropdown">
-                              <button className="dropbtn"
+                              <button
+                                className="dropbtn"
                                 onClick={e => {
                                   e.preventDefault();
 
@@ -91,23 +117,26 @@ class Nav extends React.Component {
                                       commClicked: false
                                     });
                                   }
-                                }}>Community</button>
+                                }}
+                              >
+                                Community
+                              </button>
                               <div
                                 className={`dropdown-content ${
                                   this.state.commClicked ? "reveal" : "hide"
-                                  }`}
+                                }`}
                               >
                                 <div className="dropdown-item">
-                                  <Link to='/group'>Groups</Link>
+                                  <Link to="/group">Groups</Link>
                                 </div>
                                 <div className="dropdown-item">
-                                  <Link to='/topic'>Discussions</Link>
+                                  <Link to="/topic">Discussions</Link>
                                 </div>
                                 <div className="dropdown-item">
-                                  <Link to='/quotes'>Quotes</Link>
+                                  <Link to="/quotes">Quotes</Link>
                                 </div>
                                 <div className="dropdown-item">
-                                  <Link to='/best_reviewers'>People</Link>
+                                  <Link to="/best_reviewers">People</Link>
                                 </div>
                               </div>
                             </div>
@@ -115,27 +144,34 @@ class Nav extends React.Component {
                         </ul>
                       </nav>
 
-                      <form className='search'>
-                        <input type="text" placeholder="Search books"/>
+                      <form className="search">
+                        <input type="text" placeholder="Search books" />
                         <button type="submit">
                           <i className="fa fa-search"></i>
                         </button>
                       </form>
 
-                      <div className='personal-nav'>
-                        <ul className='personal-nav-list'>
-                          <li className='personal-nav-list-item'>
-                            <Link to='/group' className='personal-link'><i className='far fa-comments'></i></Link>
+                      <div className="personal-nav">
+                        <ul className="personal-nav-list">
+                          <li className="personal-nav-list-item">
+                            <Link to="/group" className="personal-link">
+                              <i className="far fa-comments"></i>
+                            </Link>
                           </li>
-                          <li className='personal-nav-list-item'>
-                            <Link to='/inbox' className='personal-link'><i className="fa fa-envelope-o"></i></Link>
+                          <li className="personal-nav-list-item">
+                            <Link to="/inbox" className="personal-link">
+                              <i className="fa fa-envelope-o"></i>
+                            </Link>
                           </li>
-                          <li className='personal-nav-list-item'>
-                            <Link to='/friend' className='personal-link'><i className='fas fa-user-friends'></i></Link>
+                          <li className="personal-nav-list-item">
+                            <Link to="/friend" className="personal-link">
+                              <i className="fas fa-user-friends"></i>
+                            </Link>
                           </li>
-                          <li className='personal-nav-list-item'>
+                          <li className="personal-nav-list-item">
                             <div className="profile-dropdown">
-                              <button className="profile-dropbtn"
+                              <button
+                                className="profile-dropbtn"
                                 onClick={e => {
                                   e.preventDefault();
 
@@ -150,42 +186,51 @@ class Nav extends React.Component {
                                       userClicked: false
                                     });
                                   }
-                                }}><i className="fa fa-user-circle-o"></i></button>
+                                }}
+                              >
+                                <i className="fa fa-user-circle-o"></i>
+                              </button>
                               <div
                                 className={`profile-dropdown-content ${
                                   this.state.userClicked ? "reveal" : "hide"
-                                  }`}
+                                }`}
                               >
                                 <div className="profile-dropdown-item">
-                                  <Link to='/'>Profile</Link>
+                                  <Link to="/">Profile</Link>
                                 </div>
                                 <div className="profile-dropdown-item">
-                                  <Link to='/friend'>Friends</Link>
+                                  <Link to="/friend">Friends</Link>
                                 </div>
                                 <div className="profile-dropdown-item">
-                                  <Link to='/group'>Groups</Link>
+                                  <Link to="/group">Groups</Link>
                                 </div>
                                 <div className="profile-dropdown-item">
-                                  <Link to='/topic'>Discussions</Link>
+                                  <Link to="/topic">Discussions</Link>
                                 </div>
                                 <div className="profile-dropdown-item">
-                                  <Link to='/'>Comments</Link>
+                                  <Link to="/">Comments</Link>
                                 </div>
                                 <div className="profile-dropdown-item">
-                                  <Link to='/quotes'>Quotes</Link>
+                                  <Link to="/quotes">Quotes</Link>
                                 </div>
                                 <div className="profile-dropdown-item">
-                                  <Link to='/edit_fav_genres'>Favorite genres</Link>
+                                  <Link to="/edit_fav_genres">
+                                    Favorite genres
+                                  </Link>
                                 </div>
                                 <div className="profile-dropdown-item">
-                                  <Link to='/friend_recommendations'>Friends' recommendations</Link>
+                                  <Link to="/friend_recommendations">
+                                    Friends' recommendations
+                                  </Link>
                                 </div>
                                 <button
                                   className="profile-dropdown-button"
                                   onClick={e => {
                                     e.preventDefault();
                                     localStorage.removeItem("auth-token");
-                                    client.writeData({ data: { isLoggedIn: false } });
+                                    client.writeData({
+                                      data: { isLoggedIn: false }
+                                    });
                                     this.setState({
                                       browseClicked: false,
                                       commClicked: false,
@@ -193,7 +238,9 @@ class Nav extends React.Component {
                                     });
                                     this.props.history.push("/register");
                                   }}
-                                >Sign out</button>
+                                >
+                                  Sign out
+                                </button>
                               </div>
                             </div>
                           </li>
@@ -204,8 +251,13 @@ class Nav extends React.Component {
                 );
               } else {
                 return (
-                  <div className='navbar'>
-                    <Link to='/' className='out-logo'>bad</Link><Link to='/' className='out-logo2'>reads</Link>
+                  <div className="navbar">
+                    <Link to="/" className="out-logo">
+                      bad
+                    </Link>
+                    <Link to="/" className="out-logo2">
+                      reads
+                    </Link>
                     <Login />
                   </div>
                 );
@@ -216,6 +268,6 @@ class Nav extends React.Component {
       </ApolloConsumer>
     );
   }
-};
+}
 
 export default withRouter(Nav);
