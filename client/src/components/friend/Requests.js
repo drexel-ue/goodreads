532a49a69a299ef9882369ff83b4be60ba4d8d7d
ikesh pack <packappsforyou@gmail.com>
2019-10-07 12:01:23 -0400

    Style requests

--- client/src/components/friend/Requests.js
+++ client/src/components/friend/Requests.js
@@ -12,108 +12,111 @@ const { MAYBE_FRIENDS, FRIEND_IDS } = Queries;
 export default class Requests extends Component {
   render() {
     return (
-      <ApolloConsumer>
-        {client => {
-          const { _id } = client.readQuery({
-            query: gql`
-              query CachedUser {
-                _id
-              }
-            `
-          });
+      <div className="requests">
+        <div className="header">Requests</div>
+        <ApolloConsumer>
+          {client => {
+            const { _id } = client.readQuery({
+              query: gql`
+                query CachedUser {
+                  _id
+                }
+              `
+            });
 
-          return (
-            <Query query={FRIEND_IDS} variables={{ userId: _id }}>
-              {({ data, loading, error }) => {
-                if (loading || error) return <VanishingSpinner />;
+            return (
+              <Query query={FRIEND_IDS} variables={{ userId: _id }}>
+                {({ data, loading, error }) => {
+                  if (loading || error) return <VanishingSpinner />;
 
-                const { friendIds } = data;
+                  const { friendIds } = data;
 
-                return (
-                  <Query query={MAYBE_FRIENDS} variables={{ userId: _id }}>
-                    {({ loading, error, data, refetch }) => {
-                      if (loading || error) return <VanishingSpinner />;
+                  return (
+                    <Query query={MAYBE_FRIENDS} variables={{ userId: _id }}>
+                      {({ loading, error, data, refetch }) => {
+                        if (loading || error) return <VanishingSpinner />;
 
-                      const { maybeFriends } = data;
-                      const requests = maybeFriends.filter(
-                        maybe =>
-                          (maybe.friendIds.includes(_id) &&
-                            !friendIds.includes(maybe._id)) ||
-                          (!maybe.friendIds.includes(_id) &&
-                            friendIds.includes(maybe._id))
-                      );
+                        const { maybeFriends } = data;
+                        const requests = maybeFriends.filter(
+                          maybe =>
+                            (maybe.friendIds.includes(_id) &&
+                              !friendIds.includes(maybe._id)) ||
+                            (!maybe.friendIds.includes(_id) &&
+                              friendIds.includes(maybe._id))
+                        );
 
-                      return (
-                        <div>
-                          {requests.map((friend, index) => {
-                            let bookCount = 0;
-                            friend.shelves.forEach(
-                              shelf => (bookCount += shelf.bookIds.length)
-                            );
-                            const friendCount = friend.friendIds.length;
-                            const book = friend.currentlyReading;
+                        return (
+                          <div>
+                            {requests.map((friend, index) => {
+                              let bookCount = 0;
+                              friend.shelves.forEach(
+                                shelf => (bookCount += shelf.bookIds.length)
+                              );
+                              const friendCount = friend.friendIds.length;
+                              const book = friend.currentlyReading;
 
-                            return (
-                              <div key={index}>
-                                <div className="friend_search_result">
-                                  <div className="section_1">
-                                    <img
-                                      className="profile"
-                                      alt="profile"
-                                      src={friend.profilePhoto}
-                                    />
-                                    <div className="info">
-                                      <Link className="name" to="#">
-                                        {friend.name}
-                                      </Link>
-                                      <div className="stats_row">
-                                        <div className="book_count">{`${bookCount} books | `}</div>
-                                        <div className="friend_count">{`${friendCount} friends`}</div>
-                                      </div>
-                                      <FriendButton
-                                        onClick={refetch}
-                                        theirId={friend._id}
-                                      />
-                                    </div>
-                                  </div>
-                                  {friend.currentlyReading &&
-                                  this.props.location !== "requests" ? (
-                                    <div className="section_2">
+                              return (
+                                <div key={index}>
+                                  <div className="friend_search_result">
+                                    <div className="section_1">
                                       <img
-                                        className="cover"
-                                        alt="cover"
-                                        src={book.coverPhoto}
+                                        className="profile"
+                                        alt="profile"
+                                        src={friend.profilePhoto}
                                       />
                                       <div className="info">
-                                        <div className="currently_reading">
-                                          Currently Reading:
+                                        <Link className="name" to="#">
+                                          {friend.name}
+                                        </Link>
+                                        <div className="stats_row">
+                                          <div className="book_count">{`${bookCount} books | `}</div>
+                                          <div className="friend_count">{`${friendCount} friends`}</div>
                                         </div>
-                                        <div className="title">
-                                          <Link to={`/book/${book._id}`}>
-                                            {book.title}
-                                          </Link>
-                                        </div>
-                                        <div className="page">{`"On page ${friend.currentPage}"`}</div>
+                                        <FriendButton
+                                          onClick={refetch}
+                                          theirId={friend._id}
+                                        />
                                       </div>
                                     </div>
-                                  ) : (
-                                    <div />
-                                  )}
+                                    {friend.currentlyReading &&
+                                    this.props.location !== "requests" ? (
+                                      <div className="section_2">
+                                        <img
+                                          className="cover"
+                                          alt="cover"
+                                          src={book.coverPhoto}
+                                        />
+                                        <div className="info">
+                                          <div className="currently_reading">
+                                            Currently Reading:
+                                          </div>
+                                          <div className="title">
+                                            <Link to={`/book/${book._id}`}>
+                                              {book.title}
+                                            </Link>
+                                          </div>
+                                          <div className="page">{`"On page ${friend.currentPage}"`}</div>
+                                        </div>
+                                      </div>
+                                    ) : (
+                                      <div />
+                                    )}
+                                  </div>
                                 </div>
-                              </div>
-                            );
-                          })}
-                          <VanishingSpinner />
-                        </div>
-                      );
-                    }}
-                  </Query>
-                );
-              }}
-            </Query>
-          );
-        }}
-      </ApolloConsumer>
+                              );
+                            })}
+                            <VanishingSpinner />
+                          </div>
+                        );
+                      }}
+                    </Query>
+                  );
+                }}
+              </Query>
+            );
+          }}
+        </ApolloConsumer>
+      </div>
     );
   }
 }
